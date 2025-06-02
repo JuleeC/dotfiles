92032ef9417078cdabb7915035bb4941cae01494
JuleeC <juleshummel44@gmail.com>
2025-06-02 22:42:38 +0000

    oil

--- .config/nvim/lua/julee/packer.lua
+++ .config/nvim/lua/julee/packer.lua
@@ -7,28 +7,25 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
 -- or                            , branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
   }
   use 'AlexvZyl/nordic.nvim'
-
-  
   use ('nvim-treesitter/nvim-treesitter',{ run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
   use ('ThePrimeagen/harpoon') 
   use ('mbbill/undotree') 
   use ('tpope/vim-fugitive') 
   use({"nvim-lua/plenary.nvim"})
+  use 'nvim-tree/nvim-web-devicons' -- Recommended for icons
   use {
     'stevearc/oil.nvim',
     config = function()
       require('after/plugin.oil')  -- Assuming oil.lua is in lua/config/
     end
   }
-
   use {
   "startup-nvim/startup.nvim",
   requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim"},

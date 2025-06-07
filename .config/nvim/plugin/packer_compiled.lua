0602ea1f65e7cd5d8b8c141d328ceb6d82ad3908
JuleeC <juleshummel44@gmail.com>
2025-06-07 12:53:54 +0000

    huh

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -125,6 +125,16 @@ _G.packer_plugins = {
     path = "/home/julee/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/julee/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/julee/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   ["mini.icons"] = {
     loaded = true,
     path = "/home/julee/.local/share/nvim/site/pack/packer/start/mini.icons",

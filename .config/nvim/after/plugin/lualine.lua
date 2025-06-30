72118093d6e800be82654fbd8e1933e5e46621fc
JuleeC <juleshummel44@gmail.com>
2025-06-30 12:28:03 +0000

    why does coc make problems

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -8,22 +8,21 @@ require('lualine').setup({
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'location'},
-   -- lualine_c = {'filename',{lsp.lsp_client },},
-    lualine_x = {'diagnostics', 'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'location' },
+    -- lualine_c = {'filename',{lsp.lsp_client },},
+    lualine_x = { 'diagnostics', 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'diagnostics' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
   extensions = {}
 })
-

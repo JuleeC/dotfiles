aea3009311554f3842c103527645705accc5e9cf
JuleeC <juleshummel44@gmail.com>
2025-07-13 13:50:30 +0000

    what did i do

--- .config/nvim/lua/julee/remap.lua
+++ .config/nvim/lua/julee/remap.lua
@@ -6,8 +6,6 @@ vim.keymap.set("n", "<CR>", ":noh<CR>", { desc = "cleasr search highlight" })
 
 
 vim.keymap.set("i", "ö", "(", { desc = "replacing ö with (" })
-vim.keymap.set("n", "ö", "(", { desc = "replacing ö with ( in normal mode" })
 
 
 vim.keymap.set("i", "Ö", "(", { desc = "replacing ö with )" })
-vim.keymap.set("n", "Ö", "(", { desc = "replacing ö with ) in normal mode" })

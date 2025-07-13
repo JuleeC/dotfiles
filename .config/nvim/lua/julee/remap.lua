4835779af59a8d14412299e7ad10be318c7bae95
JuleeC <juleshummel44@gmail.com>
2025-07-13 13:45:19 +0000

    remapping

--- .config/nvim/lua/julee/remap.lua
+++ .config/nvim/lua/julee/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Open netrw" })
+vim.keymap.set("n", "<CR>", ":noh<CR>", { desc = "cleasr search highlight" })
+vim.keymap.set("i", "ö", "(", { desc = "replacing ö with (" })

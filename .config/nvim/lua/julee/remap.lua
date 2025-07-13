cdc38dac97c514d9e5cdae9ef387f1f480051650
JuleeC <juleshummel44@gmail.com>
2025-07-13 13:52:53 +0000

    chanigng braces

--- .config/nvim/lua/julee/remap.lua
+++ .config/nvim/lua/julee/remap.lua
@@ -1,11 +1,14 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Open netrw" })
 
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = "Open netrw" })
 
 vim.keymap.set("n", "<CR>", ":noh<CR>", { desc = "cleasr search highlight" })
 
-
+--- remapping the (ö) key and {ä} key
 vim.keymap.set("i", "ö", "(", { desc = "replacing ö with (" })
 
+vim.keymap.set("i", "Ö", ")", { desc = "replacing ö with )" })
 
-vim.keymap.set("i", "Ö", "(", { desc = "replacing ö with )" })
+vim.keymap.set("i", "ä", "{", { desc = "replacing ö with {" })
+
+vim.keymap.set("i", "Ä", "}", { desc = "replacing ö with }" })
